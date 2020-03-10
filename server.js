cf9ea640e48cd4ea617028c766b0871327e0d005
Dmytro Nechepurenko <dimanechepurenko@gmail.com>
2020-03-10 11:58:18 +0200

    ADD: headers

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const pathUtil = path;
 const http2 = require('http2');
 const connect = require('connect');
 const serveStatic = require('serve-static');
@@ -9,8 +10,14 @@ const certs = {
 	cert: fs.readFileSync(__dirname + '/certs/devserv.crt', 'utf8')
 };
 
+function setHeaders (res, path) {
+	if (pathUtil.extname(path) === '.js') {
+		res.setHeader('Content-Type', 'application/javascript')
+	}
+}
+
 const app = connect('https://127.0.0.1');
-app.use(serveStatic(path.join(__dirname, 'public')));
+app.use(serveStatic(path.join(__dirname, 'public'), { 'setHeaders': setHeaders }));
 app.listen();
 http2.createSecureServer(certs, app).listen(process.env.PORT || port);
 
