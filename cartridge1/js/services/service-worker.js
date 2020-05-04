6258ca3ddc2c52a26319469d25b9d8d917990bc7
Dmytro Nechepurenko <dimanechepurenko@gmail.com>
2020-05-04 22:41:00 +0300

    UP: server

--- cartridge1/js/services/service-worker.js
+++ cartridge1/js/services/service-worker.js
@@ -1,5 +1,3 @@
-self.addEventListener('install', () => self.skipWaiting());
-
 // Message Relay
 self.addEventListener('message', function(event) {
 	const forwardMessage = self.clients.matchAll()
@@ -24,9 +22,16 @@ self.addEventListener('message', function(event) {
 	}
 });
 
-// Immediately claim any new clients. This is not needed to send messages, but
-// makes for a better experience since the user does not need to refresh.
+self.addEventListener('install', function(event) {
+	// Bypass the waiting lifecycle stage,
+	// just in case there's an older version of this SW registration.
+	event.waitUntil(self.skipWaiting());
+});
+
 self.addEventListener('activate', (event) => {
+	// Take control of all pages under this SW's scope immediately,
+	// instead of waiting for reload/navigation.
+
 	// Debug
 	//self.clients.matchAll({
 	//	includeUncontrolled: true
--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 const fs = require('fs');
 const path = require('path');
 const url = require('url');
-const http2 = require('http2');
 const connect = require('connect');
 const serveStatic = require('serve-static');
 const bodyParser = require('body-parser');
 const port = 3000;
+const http2 = require('http2');
 const certs = {
 	key: fs.readFileSync(__dirname +  '/.certs/localhost.key', 'utf8'),
 	cert: fs.readFileSync(__dirname + '/.certs/localhost.crt', 'utf8')
 };
 
 const app = connect('127.0.0.1');
-app.use(serveStatic(path.join(__dirname, 'pages')));
+
 app.use(serveStatic(path.join(__dirname, 'cartridge1')));
 app.use(serveStatic(path.join(__dirname, 'cartridge2')));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use('/service-worker.js', function(req, res) {
+	res.writeHead(200, {'Content-Type': 'application/javascript'});
+	res.end(fs.readFileSync('./cartridge1/js/services/service-worker.js'));
+});
 
 app.use('/plp', function(req, res) {
 	const queryObject = url.parse(req.url, true).query;
@@ -37,8 +45,6 @@ app.use('/plp', function(req, res) {
 	res.end(html);
 });
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
 app.use('/endpoint', function(req, res) {
 	const request = req.body;
 	let response = {success: 'success message'};
@@ -75,6 +81,11 @@ app.use('/endpoint', function(req, res) {
 	res.writeHead(head.code, head.contentType);
 	const isTextType = request.expectedResponse === 'notJSON';
 	res.end(isTextType ? response : JSON.stringify(response));
+});
+
+app.use('/', function(req, res) {
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.end(fs.readFileSync('./index.html'));
 });
 
 app.listen();
