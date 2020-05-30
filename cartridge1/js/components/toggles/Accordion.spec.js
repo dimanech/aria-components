dba1d917c8a1dcc5a0c7457388aff3c3972b156e
Dmytro Nechepurenko <dimanechepurenko@gmail.com>
2020-05-30 18:02:43 +0300

    WIP: mocha reports

--- cartridge1/js/components/toggles/Accordion.spec.js
+++ cartridge1/js/components/toggles/Accordion.spec.js
@@ -3,31 +3,35 @@ describe('Accordion', async () => {
 
 	before(async () => {
 		page = await global.browser.newPage();
-		await page.goto("http://127.0.0.1:3000/js/components/toggles/accordion.html");
-		await page.setViewport({ width: 1920, height: 1040 });
+		// await page.emulate(iPhone);
+		await page.goto("http://127.0.0.1:3000/js/components/toggles/accordion.html", { waitUntil: 'networkidle2'});
+		await page.setViewport({ width: 1920, height: 1080 });
+		await page.coverage.startJSCoverage();
 	});
 
 	after(async () => {
+		const jsCoverage = await page.coverage.stopJSCoverage();
+		global.pti.write([...jsCoverage]);
 		await page.close();
 	});
 
 	describe('Structure', function () {
 		it('should have proper roles', async function () {
 			// https://pptr.dev/#?product=Puppeteer&version=v3.1.0&show=api-accessibilitysnapshotoptions
-			const snapshot = await page.accessibility.snapshot();
+			//const snapshot = await page.accessibility.snapshot();
 			//console.log(snapshot);
 		});
 	})
 
-	describe.only('Toggle panel', function () {
+	describe('Toggle panel', function () {
 		let firstButtonHandle;
 		let firstButtonControlledElementID;
 		let secondButtonHandle;
 
 		before(async () => {
-			const controlls = await page.$$('.accordion__control');
-			firstButtonHandle = controlls[0];
-			secondButtonHandle = controlls[1];
+			const buttons = await page.$$('.accordion__control');
+			firstButtonHandle = buttons[0];
+			secondButtonHandle = buttons[1];
 			firstButtonControlledElementID = '#' + await firstButtonHandle.evaluate(node => node.getAttribute('aria-controls'));
 		});
 
@@ -53,6 +57,33 @@ describe('Accordion', async () => {
 			await secondButtonHandle.click();
 			global.assert.strictEqual(await page.$eval(firstButtonControlledElementID, node => node.getAttribute('aria-hidden')), 'true');
 			global.assert.strictEqual(await page.$eval('.accordion__control', node => node.getAttribute('aria-expanded')), 'false');
+		});
+
+		it('should do nothing when destroyed', async function () {
+			const panelState = await page.$eval(firstButtonControlledElementID, node => node.getAttribute('aria-hidden'));
+			const controlState = await page.$eval('.accordion__control', node => node.getAttribute('aria-expanded'));
+
+			await page.evaluate(() => window.testedComponent.destroy());
+			await firstButtonHandle.click();
+
+			global.assert.strictEqual(await page.$eval(firstButtonControlledElementID, node => node.getAttribute('aria-hidden')), panelState);
+			global.assert.strictEqual(await page.$eval('.accordion__control', node => node.getAttribute('aria-expanded')), controlState);
+		});
+	})
+
+	describe('Toggle height', function () {
+		let firstButtonHandle;
+		let firstButtonControlledElementID;
+
+		before(async () => {
+			const controls = await page.$$('.accordion__control');
+			firstButtonHandle = controls[0];
+			firstButtonControlledElementID = '#' + await firstButtonHandle.evaluate(node => node.getAttribute('aria-controls'));
+		});
+
+		it('height should be cleared after destroy', async function () {
+			await page.evaluate(() => window.testedComponent.destroy());
+			global.assert.strictEqual(await page.$eval(firstButtonControlledElementID, node => node.style.height), 'auto');
 		});
 	})
 
---
+++ tests-setup.js
@@ -0,0 +1,24 @@
+const puppeteer = require('puppeteer');
+const assert = require('assert');
+const pti = require('puppeteer-to-istanbul');
+
+const width = 1920;
+const height = 1080;
+
+//const devices = require('puppeteer/DeviceDescriptors');
+//const iPhone = devices['iPhone 6'];
+
+before(async () => {
+	require('./server.js');
+
+	global.assert = assert;
+	global.browser = await puppeteer.launch({
+		args: [`--window-size=${width},${height}`]
+	});
+	global.pti = pti;
+});
+
+after(() => {
+	global.browser.close();
+	process.exit(0);
+});
